ba71e5fe6df1f55285c05cc10ef045e9ea857c7d
Muhammad Zamin <muhammad.zamin2005@gmail.com>
2025-02-11 09:01:34 +0500

    Update q2.cpp

--- Lab 2/q2.cpp
+++ Lab 2/q2.cpp
@@ -3,22 +3,28 @@ using namespace std;
 
 void* sum (void* arr, int size){
     int sum = 0;
-	int *ptr = (int*)arr;
-	for (int i = 0; i < size; i++)
+    int *ptr = (int*)arr;
+    for (int i = 0; i < size; i++)
     {
         sum += *ptr;
         ptr++;
     }
-	void *ret = &sum;
-	return (void*)ret;
+    void* out = (void*)&sum;
+    return (void*)out;
 }
 
 int main(){
-    int size = 5;
+    int size = 6;
     int arr[size];
     for (int i = 0; i < size; i++)
     {
         cout << "Element " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    
+    void* out =sum((void*)arr, size);
+    cout<< "Sum: " <<*(int*)out;
+}
         cin >> arr[i];
     }
     
